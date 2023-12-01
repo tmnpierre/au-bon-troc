4a1c3e26d902a29a4b67c78a7b2601b3c64636ab
tmnpierre <tomyn.pierre@gmail.com>
2023-12-01 22:19:43 +0100

    feat(swagger): integrate Swagger for API documentation and annotate Users routes and DTOs

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,12 @@
 import { IsString, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'user123', description: 'The pseudo of the user' })
   @IsString()
   readonly Pseudo: string;
 
+  @ApiProperty({ example: 'user@example.com', description: 'The email of the user' })
   @IsEmail()
   readonly Mail: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,38 +2,57 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({ status: 201, description: 'User successfully created.', type: CreateUserDto })
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     const user = await this.usersService.create(createUserDto);
     return { message: 'User successfully created', data: user };
   }
   
+  
+  @ApiOperation({ summary: 'Retrieve all users' })
+  @ApiResponse({ status: 200, description: 'Users fetched successfully.' })
   @Get()
   async findAll() {
     const users = await this.usersService.findAll();
     return { message: 'Users fetched successfully', data: users };
   }
   
+  @ApiOperation({ summary: 'Retrieve a user by UUID' })
+  @ApiParam({ name: 'UUID', type: 'string', required: true, description: 'User UUID' })
+  @ApiResponse({ status: 200, description: 'User fetched successfully.' })
   @Get(':UUID')
   async findOne(@Param('UUID') UUID: string) {
     const user = await this.usersService.findOne(UUID);
     return { message: 'User fetched successfully', data: user };
   }
   
+  
+  @ApiOperation({ summary: 'Update a user by UUID' })
+  @ApiParam({ name: 'UUID', type: 'string', required: true, description: 'User UUID' })
+  @ApiResponse({ status: 200, description: 'User updated successfully.' })
   @Patch(':UUID')
   async update(@Param('UUID') UUID: string, @Body() updateUserDto: UpdateUserDto) {
     const user = await this.usersService.update(UUID, updateUserDto);
     return { message: 'User updated successfully', data: user };
   }
   
+  
+  @ApiOperation({ summary: 'Delete a user by UUID' })
+  @ApiParam({ name: 'UUID', type: 'string', required: true, description: 'User UUID' })
+  @ApiResponse({ status: 200, description: 'User deleted successfully.' })
   @Delete(':UUID')
   async remove(@Param('UUID') UUID: string) {
     await this.usersService.remove(UUID);
     return { message: 'User deleted successfully' };
-  }  
+  }
+  
 }
